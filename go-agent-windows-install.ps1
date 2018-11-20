d35d08eb4ded618a6b5abd8eda75006566a04362
Prateek Baheti <prateekb@thoughtworks.com>
2018-11-20 16:26:45 +0530

    Terminating statements with semicolon Adding debugging statements

--- go-agent-windows-install.ps1
+++ go-agent-windows-install.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop';
 
 function Wait-For-Agent-Installation
 {
@@ -17,33 +17,39 @@ function Wait-For-Agent-Installation
 }
 
 if ($args.length -lt 5) {
-  throw "Must have 5/6 args: server_url server_version autoregister_key plugin_id agent_id environment"
+  throw "Must have 5/6 args: server_url server_version autoregister_key plugin_id agent_id environment";
 }
 
-$server_url = $args[0]
-$server_version = $args[1]
-$autoregister_key= $args[2]
-$plugin_id = $args[3]
-$agent_id = $args[4]
-$environment = $args[5]
-$install_dir = 'C:\go-agent'
-$install_cmd = ".\go-agent-$server_version-jre-64bit-setup.exe /S /SERVERURL=```"$server_url```" /START_AGENT=NO /D=$install_dir"
+$server_url = $args[0];
+$server_version = $args[1];
+$autoregister_key= $args[2];
+$plugin_id = $args[3];
+$agent_id = $args[4];
+$environment = $args[5];
 
-Write-Host "Disabling User Access Control"
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "0"
+Write-Host "server_url: $server_url";
+Write-Host "server_version: $server_version";
+Write-Host "plugin_id: $plugin_id";
+Write-Host "agent_id: $agent_id";
+Write-Host "environment: $environment";
+$install_dir = 'C:\go-agent';
+$install_cmd = ".\go-agent-$server_version-jre-64bit-setup.exe /S /SERVERURL=```"$server_url```" /START_AGENT=NO /D=$install_dir";
 
-Write-Host "Installing go-agent using command: $install_cmd"
-Invoke-expression $install_cmd
+Write-Host "Disabling User Access Control";
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "0";
 
-Wait-For-Agent-Installation
+Write-Host "Installing go-agent using command: $install_cmd";
+Invoke-expression $install_cmd;
+
+Wait-For-Agent-Installation;
 
 Write-Host "Adding autoregister.properties file"
 $file_content = "agent.auto.register.key=$autoregister_key`n
 agent.auto.register.environments=$environment`n
 agent.auto.register.elasticAgent.pluginId=$plugin_id`n
-agent.auto.register.elasticAgent.agentId=$agent_id"
+agent.auto.register.elasticAgent.agentId=$agent_id";
 
-$file_content | Out-File "$install_dir\config\autoregister.properties"
+$file_content | Out-File "$install_dir\config\autoregister.properties";
 
-Write-Host "Starting go-agent..."
-Start-service "Go Agent"
+Write-Host "Starting go-agent...";
+Start-service "Go Agent";
